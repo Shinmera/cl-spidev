5f85b88b3fa11c076e7b949ff99f3f94591a49a8
Shinmera <shinmera@tymoon.eu>
2017-12-22 13:36:55 +0100

    Fix TRANSMIT in high-level wrapper

--- wrapper.lisp
+++ wrapper.lisp
@@ -76,7 +76,9 @@
 (defun transmit (handle bytes &key speed delay bits/word)
   (let ((bytes #+(or sbcl ccl) bytes
                #-(or sbcl ccl) (replace (cffi:make-shareable-byte-vector (length bytes)) bytes)))
-    (cl-spidev-lli:transmit handle bytes
-                            (or speed (max-speed handle))
-                            (* 1000000 (or delay 0))
-                            (or bits/word (bits/word handle)))))
+    (cl-spidev-lli:transmit
+     (handle-stream handle)
+     bytes
+     (or speed (max-speed handle))
+     (* 1000000 (or delay 0))
+     (or bits/word (bits/word handle)))))
